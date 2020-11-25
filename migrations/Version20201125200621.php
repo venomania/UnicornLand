097007315e7510daca59f93124f5b3047b3a16ca
veno <bruno.martinsvilasboas@ynov.com>
2020-11-25 22:37:37 +0100

    css show articles + pagination

--- migrations/Version20201125200621.php
+++ migrations/Version20201125200621.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201120111308 extends AbstractMigration
+final class Version20201125200621 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -30,7 +30,6 @@ final class Version20201120111308 extends AbstractMigration
         $this->addSql('CREATE TABLE roles_users (roles_id INT NOT NULL, users_id INT NOT NULL, INDEX IDX_3D80FB2C38C751C4 (roles_id), INDEX IDX_3D80FB2C67B3B43D (users_id), PRIMARY KEY(roles_id, users_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE states (id INT AUTO_INCREMENT NOT NULL, state VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, mail VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E95126AC48 (mail), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE users_roles (users_id INT NOT NULL, roles_id INT NOT NULL, INDEX IDX_51498A8E67B3B43D (users_id), INDEX IDX_51498A8E38C751C4 (roles_id), PRIMARY KEY(users_id, roles_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_BFDD316812469DE2 FOREIGN KEY (category_id) REFERENCES categories (id)');
         $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_BFDD3168A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE Article_User_Like ADD CONSTRAINT FK_B743391EBAF6CC FOREIGN KEY (articles_id) REFERENCES articles (id) ON DELETE CASCADE');
@@ -42,8 +41,6 @@ final class Version20201120111308 extends AbstractMigration
         $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962AA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE roles_users ADD CONSTRAINT FK_3D80FB2C38C751C4 FOREIGN KEY (roles_id) REFERENCES roles (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE roles_users ADD CONSTRAINT FK_3D80FB2C67B3B43D FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE users_roles ADD CONSTRAINT FK_51498A8E67B3B43D FOREIGN KEY (users_id) REFERENCES users (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE users_roles ADD CONSTRAINT FK_51498A8E38C751C4 FOREIGN KEY (roles_id) REFERENCES roles (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema) : void
@@ -54,14 +51,12 @@ final class Version20201120111308 extends AbstractMigration
         $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962A7294869C');
         $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_BFDD316812469DE2');
         $this->addSql('ALTER TABLE roles_users DROP FOREIGN KEY FK_3D80FB2C38C751C4');
-        $this->addSql('ALTER TABLE users_roles DROP FOREIGN KEY FK_51498A8E38C751C4');
         $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962A5D83CC1');
         $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_BFDD3168A76ED395');
         $this->addSql('ALTER TABLE Article_User_Like DROP FOREIGN KEY FK_B7433967B3B43D');
         $this->addSql('ALTER TABLE Article_User_Share DROP FOREIGN KEY FK_E2C7236767B3B43D');
         $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962AA76ED395');
         $this->addSql('ALTER TABLE roles_users DROP FOREIGN KEY FK_3D80FB2C67B3B43D');
-        $this->addSql('ALTER TABLE users_roles DROP FOREIGN KEY FK_51498A8E67B3B43D');
         $this->addSql('DROP TABLE articles');
         $this->addSql('DROP TABLE Article_User_Like');
         $this->addSql('DROP TABLE Article_User_Share');
@@ -72,6 +67,5 @@ final class Version20201120111308 extends AbstractMigration
         $this->addSql('DROP TABLE roles_users');
         $this->addSql('DROP TABLE states');
         $this->addSql('DROP TABLE users');
-        $this->addSql('DROP TABLE users_roles');
     }
 }
--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,17 +7,25 @@ use App\Repository\ArticlesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface; 
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/accueil", name="accueil", methods={"GET"})
      */
-    public function index(ArticlesRepository $articlesRepository): Response
+    public function index(Request $request ,PaginatorInterface $paginator): Response
     {
+        $donnees = $this->getDoctrine()->getRepository(Articles::class)->findAll();
+        $articles = $paginator->paginate(
+            $donnees,
+            $request->query->getInt('page',1),
+            10
+        );
+
         return $this->render('accueil/index.html.twig', [
-            'articles' => $articlesRepository->findAll(),
+            'articles' => $articles,
         ]);
     }
 }
